a12e472f6d449a5e45b07c2562ca91fb8bbb79e0
Norman-Ben <90580315+Norman-Ben@users.noreply.github.com>
2023-01-18 18:38:08 +0000

    add navigation menu and toggle functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,45 +1,55 @@
-import React from 'react';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </button>
+    <>
+      <div className="navbar bg-base-100">
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost" onClick={toggleMenu}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">
+            Grand Prix<span className="text-info">.DB</span>
+          </a>
+        </div>
+        <div className="flex-none"></div>
       </div>
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-      </div>
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>
-          </svg>
-        </button>
-      </div>
-    </div>
+
+      <ul
+        className={`menu bg-base-300 w-56 rounded-box ${
+          isOpen ? 'absolute' : 'hidden'
+        }`}
+      >
+        <li>
+          <a>F1 News</a>
+        </li>
+        <li>
+          <a>Current Season</a>
+        </li>
+        <li>
+          <a>Previous Seasons</a>
+        </li>
+      </ul>
+    </>
   );
 }
