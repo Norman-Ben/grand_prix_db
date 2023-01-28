09e162dfe51b27579286f7507a60f9f17cb7c931
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2023-01-28 15:02:12 +0000

    set up basic caching for news articles with redis

--- pages/api/getNews.ts
+++ pages/api/getNews.ts
@@ -12,16 +12,46 @@ export default async function getNews(res: any) {
     articleurl: { type: 'string' },
   });
 
-  const newsRepository = client.fetchRepository<News>(newsSchema);
+  let isCached = await client.execute(['KEYS', 'News:*']);
+  // console.log(isCached);
 
-  const newNews = newsRepository.createEntity();
-  newNews.title = 'Hello World';
-  newNews.description = 'This is a description';
+  if (isCached.length === 0) {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-BingApis-SDK': 'true',
+        'X-RapidAPI-Key': 'd9067d268fmsh141632809016681p1983a2jsn7498276439f8',
+        'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+      },
+    };
 
-  const id = await newsRepository.save(newNews);
+    let bingNewsApiCall = await fetch(
+      'https://bing-news-search1.p.rapidapi.com/news/search?q=Formula%201&count=6&freshness=Day&originalImg=true&textFormat=Raw&safeSearch=Off',
+      options
+    )
+      .then((response) => response.json())
+      .catch((err) => console.error(err));
 
-  console.log(id);
-  await newsRepository.expire(id, 600);
+    console.log(bingNewsApiCall);
 
+    const newsRepository = client.fetchRepository<News>(newsSchema);
+
+    const newNews = newsRepository.createEntity();
+    newNews.title = bingNewsApiCall.value[0].name;
+    newNews.description = bingNewsApiCall.value[0].description;
+    newNews.imageurl = bingNewsApiCall.value[0].image.thumbnail.contentUrl;
+    newNews.articleurl = bingNewsApiCall.value[0].url;
+
+    const id = await newsRepository.save(newNews);
+
+    // console.log(id);
+    await newsRepository.expire(id, 600);
+  } else {
+    const newsCache = await client.execute(['KEYS', 'News:*']);
+    console.log(newsCache);
+    res = await client.execute(['JSON.GET', newsCache]);
+    console.log(res);
+  }
+  // console.log(res);
   await client.close();
 }
