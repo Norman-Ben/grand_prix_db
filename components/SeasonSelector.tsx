cf4c96b01d41ed7dde5baf6a307eb75ba13903ed
Ben Norman <90580315+Norman-Ben@users.noreply.github.com>
2023-06-30 18:17:35 +0100

    Create Season DB page with season selector

---
+++ components/SeasonSelector.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function SeasonSelect({ season, setSeason }) {
+  const handleChange = (e) => {
+    setSeason(e.target.value);
+  };
+  console.log(season);
+
+  return (
+    <select
+      className="select select-secondary w-full max-w-[180px] mt-6"
+      onChange={handleChange}
+    >
+      <option disabled selected>
+        Select a season
+      </option>
+      <option>2021</option>
+      <option>2020</option>
+      <option>2019</option>
+      <option>2018</option>
+      <option>2017</option>
+      <option>2016</option>
+      <option>2015</option>
+      <option>2014</option>
+      <option>2013</option>
+      <option>2012</option>
+    </select>
+  );
+}
